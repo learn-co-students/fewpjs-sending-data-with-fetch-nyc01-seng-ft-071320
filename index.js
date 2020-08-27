7e9290c079eb0750602415d4918700f133f3e0ee
junko911 <junkotahara911@gmail.com>
2020-08-27 17:17:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const p = document.createElement('p')
+const h1 = document.createElement('h1')
+const bodyTag = document.querySelector('body')
+
+function submitData(name, email) {
+  const data = {
+    "name": name,
+    "email": email
+  }
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(object => {
+      h1.textContent = object["id"]
+      bodyTag.append(h1)
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!")
+      p.textContent = error
+      bodyTag.append(p)
+    });
+}
