856be3f556aa0cb27d97b89779c006927cddfcfe
Mimioj101 <mimioj@gmail.com>
2020-08-30 16:37:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+document.querySelector("DOMContentLoaded", function(e) {
+  
+    function submitData(username, useremail) {
+        
+        fetch("http://localhost:3000/users", configurationObject)
+        .then(resp => resp.json())
+        .then(data => {document.body.innerHTML = data["id"]})
+
+
+
+
+        const configurationObject = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                useremail
+        })}
+    
+
+    }
+
+
+
+
+submitData() 
+
+}
